f921df83ce310f6f33c6bdc85082852652d179d0
RonnaNl <102194389+RonnaNl@users.noreply.github.com>
2022-03-23 02:26:21 +0200

    Update index.js
    TEST

--- index.js
+++ index.js
@@ -1,58 +1,26 @@
-#section * {
-    margin: 0;
-    box-sizing: border-box;
+function Block(letter, rate, used, father, code) {
+    this.latter = latter;
+    this.rate = rate;
+    this.used = used;
+    this.father = father;
+    this.code = code;
 }
-.oval-row {
-    display: flex;
-    flex-wrap: wrap;
-}
-.w50 {
-    flex: 0 0 50%;
-}
-#section {
-    overflow: hidden;
-    margin: 20px 0;
-}
-#section .oval-row {
-    flex-direction: row-reverse;
-}
-#section .bannerImage {
-    background: url("bg.jpg");
-    background-size: cover;
-    background-position: center center;
-    background-color: #337AB7;
-}
-#section .bannerText {
-    background-color: #337AB7;
-    position: relative;
-    z-index: 1;
-    padding: 120px 40px;;
-}
-#section .bannerText .oval {
-    position: absolute;
-    background-color: #337AB7;
-    top: -50%;
-    right: 0;
-    box-shadow: 20px 8px 16px rgba(0,0,0,0.2);
-    border-radius: 50%;
-    z-index: -1;
-    content: '';
-}
-#section .bannerText h2 {
-    font-size: 48px;
-    line-height: 48px;
-    color: #FFF;
-}
-#section .bannerText p {
-    font-size: 26px;
-    line-height: 30px;
-    color: #FFF;
-}
-@media screen and (max-width: 767px) {
-    #section .oval-row {
-        display: block;
+
+function FindCode(block) {
+    
+    if (tree[block.father].code !='') {
+        block.code = tree[block.father].code + '1';
     }
-    #section .bannerText {
-        padding: 0 40px 60px 40px;
+    else {
+    if (block.letter == tree[minIndex].letter) {
+        block.code = '0';
     }
-}
+    else if (block.letter == tree[preminIndex].letter) {
+        block.code = '1';
+    }
+       else {
+    FindCode(tree[block.father]);
+    block.code = tree[block.father].code + '0';
+       }
+    }
+}        
